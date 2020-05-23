640606c0f23bae95d4f98c8185356f22ab7c4d36
codamibo <iboeters@LAPTOP-MGJ0DHHN.localdomain>
2020-05-23 12:58:25 +0200

    2nd commit

--- durak_helpers.py
+++ durak_helpers.py
@@ -2,6 +2,99 @@ import numpy as np
 
 def draw_cards(src, dest, n):
 	cards = np.random.choice(range(len(src)), n, replace = False)
-	dest = np.append(dest, src[cards])
+	print('cards', cards)
+	for i in cards:
+		dest = np.append(dest, src[i])
 	src = np.delete(src, cards)
 	return src, dest
+
+def	valid_attack(player, table):
+	valid_cards = []
+	for i in range(len(player)):
+		for j in range(len(table)):
+			if (table[j][1] == player[i][1]):
+				valid_cards.append(i)
+	return valid_cards
+
+
+def	valid_defense(player, to_defend, kozur):
+	valid_cards = []
+	for i in range(len(player)):
+		if to_defend[0] != kozur:
+			if ((to_defend[1] < player[i][1]) and to_defend[0] == player[i][0]) or player[i][0] == kozur:
+				valid_cards.append(i)
+		elif ((to_defend[1] < player[i][1]) and to_defend[0] == player[i][0]):
+			valid_cards.append(i)
+	return valid_cards
+
+def inputNumber(valid_cards):
+	while True:
+		try:
+			card_index = int(input())       
+		except ValueError:
+			print("Not an integer! Try again.")
+			continue
+		else:
+			if card_index in valid_cards or card_index == -1:
+				return card_index
+			print("Not a valid card! Try again.")
+			continue
+
+def inputNumber_empty(player):
+	while True:
+		try:
+			card_index = int(input())       
+		except ValueError:
+			print("Not an integer! Try again.")
+			continue
+		else:
+			if card_index > -1 and card_index < len(player):
+				return card_index
+			print("Number not in hand! Try again.")
+			continue
+
+def game_turn(player1, player2, deck, turn, kozur_type):
+	# player 1: cards of player 1
+	# player 2: cards of player 2
+	# deck: cards remaining in the deck
+	# turn: 1 if player 1 is attacking, 2 if player 2 is attacking
+
+	turn = 1
+	table = []
+	valid_cards_at = []
+	valid_cards_def = []
+	players = [player1, player2]
+	attacker_index = (turn + 1) % 2
+	defender_index = turn % 2
+	while len(player1) and len(player2):
+		#attacker
+		print('deck', deck)
+		print('table', table)
+		print('Your options: ', players[attacker_index])
+		if len(table) > 0:
+			valid_cards_at = valid_attack(players[attacker_index], table)
+			card_index = inputNumber(valid_cards_at)
+			if card_index == -1:
+				return players[0], players[1], deck, (turn % 2) + 1	
+		else:
+			card_index = inputNumber_empty(players[attacker_index])
+		table.append(players[attacker_index][card_index])
+		players[attacker_index] = np.delete(players[attacker_index], card_index)
+
+		# defender
+		print('deck', deck)
+		print('table', table)
+		print('Your options: ', players[defender_index])
+		valid_cards_def = valid_defense(players[defender_index], table[-1], kozur_type)
+		card_index = input('For surrender type -1\n Your choice: ')
+		while card_index is int and card_index not in valid_cards_def and card_index != -1:
+			print('Not a valid move! Try again!')
+			card_index = input('For aboy type -1\n Your choice: ')
+		if card_index == -1:
+			table, players[defender_index] = draw_cards(table, players[defender_index], len(table))
+			return players[0], players[1], deck, turn
+		table.append(players[defender_index][card_index])
+		players[defender_index] = np.delete(players[defender_index], card_index)
+
+	return players[0], players[1], deck, turn
+	
--- duraki.py
+++ duraki.py
@@ -1,18 +1,30 @@
 import numpy as np
 import durak_helpers as dk
-
-SIZE_DECK = 32
+import math
+SIZE_DECK = 36
 
 # deck		0
 # player1	1
 # player2	2
 # board		3
 # bin		4
+# 0 1 2 3 4 5 6 7 8 = 6 7 8 9 10 B V H K A
+# kozur persoon start niet
+# turn wissel na aboy
+# winneerrrrr
+# error deck leeg:
+# Traceback (most recent call last):
+#   File "duraki.py", line 103, in <module>
+#     deck, player2 = dk.draw_cards(deck, player2, len(deck))
+#   File "/home/iboeters/Doerak/durak_helpers.py", line 4, in draw_cards
+#     cards = np.random.choice(range(len(src)), n, replace = False)
+#   File "mtrand.pyx", line 1121, in mtrand.RandomState.choice
+# ValueError: a must be non-empty
 
-deck = np.array(['H0', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7',
-                'K0', 'K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7',
-                'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7',
-                'S0', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7'])
+deck = np.array(['H0', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8',
+                'K0', 'K1', 'K2', 'K3', 'K4', 'K5', 'K6', 'K7', 'K8',
+                'R0', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8',
+                'S0', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'])
 
 # np.random.shuffle(deck)
 print('Deck:')
@@ -22,8 +34,8 @@ player1 = np.array([])
 player2 = np.array([])
 kozur = np.array([])
 
-deck, player1 = dk.draw_cards(deck, player1, 7)
-deck, player2 = dk.draw_cards(deck, player2, 7)
+deck, player1 = dk.draw_cards(deck, player1, 6)
+deck, player2 = dk.draw_cards(deck, player2, 6)
 deck, kozur = dk.draw_cards(deck, kozur, 1)
 kozur_type = kozur[0][0]
 
@@ -41,9 +53,9 @@ print(kozur)
 print(kozur_type)
 
 turn = 0
-lowest = '8'
-lowest_p1 = '8'
-lowest_p2 = '8'
+lowest = '9'
+lowest_p1 = '9'
+lowest_p2 = '9'
 
 for card in range(len(player1)):
 	if player1[card][0] == kozur_type:
@@ -74,3 +86,29 @@ print("Lowest: ")
 print(lowest)
 print(lowest_p1)
 print(lowest_p2)
+
+total = 0
+
+while len(player1) and len(player2):
+	player1, player2, deck, turn = dk.game_turn(player1, player2, deck, turn, kozur_type)
+	if 6 - len(player1) > 0 and 6 - len(player2) > 0:
+		if (6 - len(player1) + 6 - len(player2)) > len(deck) + 1:
+			total = len(player1) + len(player2) + len(deck) + 1
+			if (turn == 1):
+				deck, player1 = dk.draw_cards(deck, player1, math.floor(total/2))
+				deck, player2 = dk.draw_cards(deck, player2, len(deck))
+				deck, player2 = dk.draw_cards(kozur, player2, 1)
+			else:
+				deck, player2 = dk.draw_cards(deck, player2, math.floor(total/2))
+				deck, player1 = dk.draw_cards(deck, player1, len(deck))
+				deck, player1 = dk.draw_cards(kozur, player1, 1)
+		else:
+			deck, player1 = dk.draw_cards(deck, player1, 6 - len(player1))
+			deck, player2 = dk.draw_cards(deck, player2, 6 - len(player2))
+	elif 6 - len(player1) > len(deck):
+		deck, player1 = dk.draw_cards(deck, player1, len(deck))
+	elif 6 - len(player2) > len(deck):
+		deck, player2 = dk.draw_cards(deck, player2, len(deck))
+	else:
+		deck, player1 = dk.draw_cards(deck, player1, max(6 - len(player1), 0))
+		deck, player2 = dk.draw_cards(deck, player2, max(6 - len(player2), 0))
